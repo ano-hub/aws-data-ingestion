6d33413248ec1a78ae5d590b821b6da9149e85cf
gunjan.shah <gunjan.shah@ihsmarkit.com>
2021-07-11 11:12:26 +0530

    vplk-gs: Added update stack statement in the script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,8 +11,10 @@
 service='Vepolink'
 capabilities=CAPABILITY_NAMED_IAM
 region=ap-south-1
-formation_type="create-stack"
-poll_type="stack-create-complete"
+crt_formation_type="create-stack"
+crt_poll_type="stack-create-complete"
+upd_formation_type="update-stack"
+upd_poll_type="stack-update-complete"
 role_arn="arn:aws:iam::828206008857:role/vplk-deploy-role-<ENV>"
 
 today_date=$(date +%d%m%Y)
@@ -47,8 +49,9 @@ checkstack() {
  echo $flag
 }
 
-
+# *****************************************************************************
 # The purpose of this subroutine is to create a new cloudformation stack.
+# *****************************************************************************
 createstack() {
 
  stack=$1
@@ -57,13 +60,78 @@ createstack() {
 
  role_arn1=$(echo "$role_arn" | sed "s/<ENV>/$env_name/g")
 
- aws cloudformation ${formation_type} --role-arn $role_arn1 --stack-name ${stack} ${templ} --capabilities ${capabilities} --region ${region} ${tags}
+ aws cloudformation ${crt_formation_type} --role-arn $role_arn1 --stack-name ${stack} ${templ} --capabilities ${capabilities} --region ${region} ${tags}
  ret_code=$?
  if [[ $ret_code -eq 0 ]]
  then
   echo "$today_date: Waiting for stack ${stack} create..."
-  aws cloudformation wait ${poll_type} --stack-name ${stack} --region ${region}
+  aws cloudformation wait ${crt_poll_type} --stack-name ${stack} --region ${region}
  fi
+}
+
+# *****************************************************************************
+# The purpose of this subroutine is to update an existing cloudformation stack.
+# *****************************************************************************
+updatestack() {
+
+ stack=$1
+ templ="--template-body file://$2"
+ tags="--tags file://$3"
+
+ flag=0
+
+ stderr=$((create_exec_change_set ${stack} $2) 2>&1)
+ ret_code=$?
+
+ if [[ $stderr =~ "No updates are to be performed" && $ret_code -eq 255 ]]
+ then
+  print("No updates are to be performed")
+ elif [[ $ret_code -ne 0 ]]
+ then
+  print("No updates are to be performed")
+ else
+  echo "$today_date: Waiting for stack ${stack_name} update..." >> ${DAAS_LOG_FILE}
+  aws cloudformation wait ${upd_poll_type} --stack-name ${stack_name} --region ${region}
+ fi
+
+}
+
+
+# *****************************************************************************
+# The purpose of this subroutine is to create a changeset with all the changes to be deployed
+# on existing Vepolink cloudformation stack and execute that changeset.
+# *****************************************************************************
+
+create_exec_change_set() {
+
+ stack=$1
+ template=$2
+ tags="--tags file://${tag_file}"
+
+ ch_poll_type='change-set-create-complete'
+
+ changesetname='changeset-'$(date +%H%M%S)
+ stderr=$(aws cloudformation create-change-set --stack-name $stack --change-set-name $changesetname --change-set-type UPDATE --template-body "file://$template" --capabilities ${capabilities} --region ${region} ${tags})
+ ret_code=$?
+
+ echo "$today_date: $stderr while creating changeset $changesetname with return code: $ret_code"
+
+ if [[ $ret_code -eq 0 ]]
+ then
+  echo "$today_date: Waiting for stack ${changesetname} create/update..."
+  stderr=$((aws cloudformation wait ${ch_poll_type} --change-set-name ${changesetname} --stack-name $stack --region ${region}) 2>&1)
+  ret_code=$?
+  if [[ $ret_code -eq 0 ]]
+  then
+   echo "$today_date: Executing the changeset $changesetname to update the stack --stack-name $stack"
+   stderr=$((aws cloudformation execute-change-set --change-set-name ${changesetname} --stack-name $stack --region ${region}) 2>&1)
+  fi
+ else
+  echo "$today_date: No updates are to be performed for ${changesetname}..."
+  #send this to the calling env as stderr
+  echo "$today_date: No updates are to be performed for ${changesetname}..."
+ fi
+
 }
 
 ##########################################
@@ -94,11 +162,11 @@ else
     template_file=`ls -1 $template_dir/param*.yaml`
     stack=$(get_stack_name $template_file $env_name)
 
-    rc=$(checkstack ${stack_name})
+    rc=$(checkstack $stack)
 
     if [[ $rc -eq 0 ]]
     then
-     echo "$today_date: Creating stack: --stack-name ${stack} for cloudformation template $file"
+     echo "$today_date: Creating stack: --stack-name ${stack} for cloudformation template $template_file"
      createstack $stack ${template_file} ${tag_file}
     else
      echo "$today_date: --stack-name ${stack} already exists..."
@@ -107,14 +175,15 @@ else
     template_file=`ls -1 $template_dir/buckets*.yaml`
     stack=$(get_stack_name $template_file $env_name)
 
-    rc=$(checkstack ${stack_name})
+    rc=$(checkstack $stack)
 
     if [[ $rc -eq 0 ]]
     then
-     echo "$today_date: Creating stack: --stack-name ${stack} for cloudformation template $file"
+     echo "$today_date: Creating stack: --stack-name ${stack} for cloudformation template $template_file"
      createstack $stack ${template_file} ${tag_file}
     else
-     echo "$today_date: --stack-name ${stack} already exists..."
+     echo "$today_date: Updating --stack-name ${stack} for cloudformation template $template_file"
+     updatestack $stack ${template_file} ${tag_file}
     fi
 
 
